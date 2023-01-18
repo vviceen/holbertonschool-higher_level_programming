ee289e5f0c7efeb5914fb3d6d23279b4075a2c82
Vicente Debrun <debrunvicente@gmail.com>
2023-01-18 11:43:02 -0800

    fix

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,4 +2,7 @@
 for iterator in range(10):
     for scnd_itr in range(0, 10):
         if iterator < scnd_itr:
-            print(str(iterator) + str(scnd_itr), end=", ")
+            if iterator == 8 and scnd_itr == 9:
+                continue
+            print("{}{}".format(iterator, scnd_itr), end=", ")
+print("89")
