d419e702ec181c9ffaf05ae6a11c7d08c42d7da8
Vicente Debrun <debrunvicente@gmail.com>
2023-01-19 06:14:47 -0800

    fix

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
-def print_last_digit(number):
-    print(number % 10)
-    return number % 10
+def print_last_digit(number): 
+    if number < 0:
+       number = -1 * (abs(number) % 10)
+    else:
+        number = number % 10
+    print(number, end="")
+    return number
