eb32bcf802c6cf1b99585cc932e79275ae0ed55f
Vicente Debrun <debrunvicente@gmail.com>
2023-01-19 09:05:41 -0800

    task 12

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for number in range(1, 101):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz ", end="")
+            continue
+        if number % 3 == 0:
+            print("Fizz ", end="")
+            continue
+        if number % 5 == 0:
+            print("Buzz ", end="")
+            continue
+        print(f"{number} ", end="")
+
+fizzbuzz()
