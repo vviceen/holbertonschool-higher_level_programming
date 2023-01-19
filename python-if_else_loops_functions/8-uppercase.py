4c519079f16d21eac6844289ce6039ff96c4a6ed
Vicente Debrun <debrunvicente@gmail.com>
2023-01-19 05:51:43 -0800

    fix fix

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,5 +3,5 @@ def uppercase(str):
     for word in str:
         if ord(word) >= 97 and ord(word) <= 122:
             word = chr(ord(word) - 32)
-            print("{}".format(word), end="")
+        print("{}".format(word), end="")
     print()
