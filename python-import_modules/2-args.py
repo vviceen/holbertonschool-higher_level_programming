8353c9d8652df7b9ab703edfa761f335021ef9db
Vicente Debrun <debrunvicente@gmail.com>
2023-01-20 12:43:59 -0800

    task 2

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__":
+    if len(argv) == 2:
+        print("1 argument:")
+        print("1: {}".format(argv[1]))
+    else:
+        print("{}: arguments".format(len(argv) - 1), end="")
+        if len(argv) == 1:
+            print(".")
+        else:
+            print(":")
+            for n in range(1, len(argv)):
+                print("{}: {}".format(n, argv[n]))
