a957cc2118c892d24b44433bb9381f754e634bb1
Vicente Debrun <debrunvicente@gmail.com>
2023-01-27 06:14:46 -0800

    :d added

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for n in range(0, len(i)):
             if n - 1 < len(i):
-                print("{}".format(i[n]), end="")
+                print("{:d}".format(i[n]), end="")
             if n - 2 < len(i):
                 print(" ", end="")
         print()
