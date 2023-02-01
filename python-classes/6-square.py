87be8901e249efef1b5081a8ea3c615382525602
Vicente Debrun <debrunvicente@gmail.com>
2023-02-01 10:44:13 -0800

    task 6

---
+++ python-classes/6-square.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+""" class Square that defines a square """
+
+
+class Square:
+    """ class Square """
+    def __init__(self, size=0, position=(0, 0)):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+        if type(position) != tuple or len(position) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = position
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__position[1] > 0:
+            for new_line in range(0, self.__position[1]):
+                print()
+
+        if self.__size == 0:
+            print()
+        for line in range(0, self.__size):
+            print("_" * self.__position[0], end="")
+            print("#" * self.__size)
+
+    @property
+    def position(self):
+        return self.__postion
+
+    @position.setter
+    def position(self, value):
+        if type(value) != tuple or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
