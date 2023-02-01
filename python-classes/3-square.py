c1c32ce6a9657302d72b07079a650fd9825e18d3
Vicente Debrun <debrunvicente@gmail.com>
2023-02-01 05:40:59 -0800

    task 3

---
+++ python-classes/3-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" class Square that defines a square """
+
+
+class Square:
+    """ class Square """
+    def __init__(self, size=0):
+        self.__size = size
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+    def area(self):
+        return self.__size ** 2
