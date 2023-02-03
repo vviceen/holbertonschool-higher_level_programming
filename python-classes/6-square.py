4175e52213b9bbb128f855450a4889e458d952d5
Vicente Debrun <debrunvicente@gmail.com>
2023-02-03 04:59:32 -0800

    1 case border solved

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -12,6 +12,8 @@ class Square:
         self.__size = size
         if type(position) != tuple or len(position) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
+        if type(position[0]) != int or type(position[1]) != int:
+            raise TypeError("position must be a tuple of 2 positive integers")
         if position[0] < 0 or position[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = position
@@ -38,6 +40,8 @@ class Square:
     @position.setter
     def position(self, value):
         if type(value) != tuple or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
