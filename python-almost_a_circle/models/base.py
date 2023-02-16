d463d8c7304558aceb4a5f77370401d0b213d1e5
Vicente Debrun <debrunvicente@gmail.com>
2023-02-16 13:52:55 -0800

    blank lines

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -5,12 +5,10 @@
 class Base:
     """ base class """
 
-
     __nb_objects = 0
 
     def __init__(self, id=None):
         """ init constructor """
-
 
         if id is not None:
             self.id = id
