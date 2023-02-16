afd0aa768a7e0eae53e8b77561ada6c7e681070b
Vicente Debrun <debrunvicente@gmail.com>
2023-02-16 12:29:41 -0800

    documentation fix

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -3,6 +3,7 @@
 
 
 def add_integer(a, b=98):
+    """ function adds 2 ints """
     if type(a) != int and type(a) != float:
         raise TypeError("a must be an integer")
     if type(b) != int and type(b) != float:
