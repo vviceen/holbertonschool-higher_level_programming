57d88fd71b1766a2725bc3403e34fa75fd6c87ba
Vicente Debrun <debrunvicente@gmail.com>
2023-03-22 21:20:10 -0700

    task 10

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" task 10 """
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ class that inherits from Rectangle """
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__width = size
+        self.__height = size
+
+    def area(self):
+        """ returns the area """
+        return self.__width * self.__height
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}" #class rectangle for checker reasons :/
