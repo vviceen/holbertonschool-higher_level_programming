5bfc056a4c1ba23757036eaaad7f743ef9a29f60
Vicente Debrun <debrunvicente@gmail.com>
2023-03-26 00:22:37 -0700

    add task 5 and tests

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" task 5"""
+
+
+def text_indentation(text):
+    """" prints a text with 2 new lines after each of these characters: ., ? and : """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    indent = [".", "?", ":"]
+    for idx, letter in enumerate(text):
+        if idx > 0 and text[idx - 1] in indent and letter not in indent:
+            continue
+        print(letter, end="")
+        if letter in indent:
+            print("\n\n", end="")
