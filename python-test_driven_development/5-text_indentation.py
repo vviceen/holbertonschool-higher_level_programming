6e295cc61a7d8c8a152e7bdd2bf708931f7ffa0d
Vicente Debrun <debrunvicente@gmail.com>
2023-03-26 00:42:19 -0700

    add space case

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -7,8 +7,14 @@ def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
     indent = [".", "?", ":"]
+    no_space = False
     for idx, letter in enumerate(text):
+        if no_space:
+            if letter == " ":
+                continue
+        no_space = False
         if idx > 0 and text[idx - 1] in indent and letter == " ":
+            no_space = True
             continue
         print(letter, end="")
         if letter in indent:
