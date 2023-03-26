427cd4910f609f580c7117a19a713413593e62dd
Vicente Debrun <debrunvicente@gmail.com>
2023-03-26 00:29:53 -0700

    fixes

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -3,12 +3,12 @@
 
 
 def text_indentation(text):
-    """" prints a text with 2 new lines after each of these characters: ., ? and : """
+    """" prints a text with 2 new lines after ., ? and : """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
     indent = [".", "?", ":"]
     for idx, letter in enumerate(text):
-        if idx > 0 and text[idx - 1] in indent and letter not in indent:
+        if idx > 0 and text[idx - 1] in indent and letter == " ":
             continue
         print(letter, end="")
         if letter in indent:
