b789000f129cd62a9f101ee428c53065228b9216
Vicente Debrun <debrunvicente@gmail.com>
2023-03-26 10:49:07 -0700

    pep8

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -6,6 +6,7 @@ class Rectangle:
     """ defines a rectangle """
 
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         if not isinstance(width, int):
             raise TypeError("width must be an integer")
