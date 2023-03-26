21ced817b903e4bb784b268f82e56190da370057
Vicente Debrun <debrunvicente@gmail.com>
2023-03-25 22:44:18 -0700

    add task 3

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" task 3"""
+
+
+def say_my_name(first_name, last_name=""):
+    """ function that prints My name is <first name> <last name> """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print(f"My name is {first_name} {last_name}")
