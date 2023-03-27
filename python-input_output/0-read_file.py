4e1626d4bd8d18f526a8b8845aec714b61c23b8c
Vicente Debrun <debrunvicente@gmail.com>
2023-03-26 22:04:43 -0700

    fix?

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -3,7 +3,7 @@
 
 
 def read_file(filename=""):
-    """ reads a text file (UTF8) and prints it to stdout """
+    """ reads a text file (UTF8) and prints it to stdout"""
     with open(filename, 'r', encoding="utf-8") as f:
         content = f.read()
-        print(content)
+        print(content, end="")
