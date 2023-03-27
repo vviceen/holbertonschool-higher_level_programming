6a46aceda13e532cf7cd5f843d175ad16bf16b44
Vicente Debrun <debrunvicente@gmail.com>
2023-03-27 02:07:58 -0700

    table fixes

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -3,7 +3,7 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY UNIQUE,
-    state_id INT NOT NULL
-    FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id)
+    state_id INT NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id),
     name VARCHAR(256) NOT NULL
 );
