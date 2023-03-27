daa1f6fbce550d11791c317725ecb1a13a8a0067
Vicente Debrun <debrunvicente@gmail.com>
2023-03-26 22:16:49 -0700

    add task 2

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" task 2 """
+
+
+def append_write(filename="", text=""):
+    """  appends a string at the end of a text file """
+    with open(filename, 'a', encoding="utf-8") as f:
+        f.write(text)
+        return len(text)
