6f37337a1e3255d2bac7df8353ab95a33be209f7
Vicente Debrun <debrunvicente@gmail.com>
2023-03-26 22:53:53 -0700

    add task 6

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" task 6 """
+import json
+
+
+def load_from_json_file(filename):
+    """ creates an Object from a JSON file """
+    with open(filename, 'r', encoding="utf-8") as f:
+        json.load(f)
