5895b3f3781c8ac63ff977d5ef274fb4804ecf94
Vicente Debrun <debrunvicente@gmail.com>
2023-03-26 22:12:12 -0700

    del trailing

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -3,7 +3,7 @@
 
 
 def write_file(filename="", text=""):
-    """ write a string to a text file 
+    """ write a string to a text file
     and returns the number of characters written """
     with open(filename, "w", encoding="utf-8") as f:
         f.write(text)
