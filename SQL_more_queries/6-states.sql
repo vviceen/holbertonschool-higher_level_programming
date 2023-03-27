2bb9f063273c84953d954b30e05f127cfc883a47
Vicente Debrun <debrunvicente@gmail.com>
2023-03-27 02:00:29 -0700

    create database fix

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,5 +1,5 @@
 -- script that creates the database and the table in the database
-CREATE DATANASE IF NOT EXISTS hbtn_0d_usa;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY UNIQUE,
