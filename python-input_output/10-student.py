5a847f9a6d62203c0cafbfe0a228a8bd7456a64c
Vicente Debrun <debrunvicente@gmail.com>
2023-03-26 23:41:28 -0700

    add task 10

---
+++ python-input_output/10-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" task 10 """
+
+
+class Student:
+    """ defines a student"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ retrieves a dictionary representation """
+        if attrs is None:
+            return self.__dict__
+        else:
+            new_dict = {}
+            for attr in attrs:
+                if hasattr(self, attr):
+                    new_dict[attr] = getattr(self, attr)
+            return new_dict
