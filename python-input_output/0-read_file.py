2e3cea594a3f7b4d039d4e2e7db4f818eb8c75ab
Vicente Debrun <debrunvicente@gmail.com>
2023-03-26 21:56:31 -0700

    add docstring

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -3,6 +3,7 @@
 
 
 def read_file(filename=""):
+    """ reads a text file (UTF8) and prints it to stdout """
     with open(filename, 'r', encoding="utf-8") as f:
         content = f.read()
         print(content)
