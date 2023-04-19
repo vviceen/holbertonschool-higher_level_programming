736a646f4e7acaf98fceeaedb997f364963780b4
Vicente Debrun <debrunvicente@gmail.com>
2023-04-19 16:07:17 -0700

    add task 10

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+console.log(factorial(process.argv[2]));
+function factorial(num) {
+  if(!num) {
+    return 1;
+  }
+  if (num === 0) {
+    return 1;
+  } else {
+    return num * factorial(num - 1);
+  }
+}
