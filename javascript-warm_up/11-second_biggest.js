bf1935109c770c1c04bd7cea244ea92329b67bb2
Vicente Debrun <debrunvicente@gmail.com>
2023-04-20 12:45:09 -0700

    fix

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
 if (args.length === 0 || args.length === 1) {
-  console.log('0')
+  console.log('0');
 } else {
   args.sort(function (a, b) {
     return b - a;
