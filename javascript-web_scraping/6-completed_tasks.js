033caa112998ac5967b0fe3bc3cdb1f27c131503
Vicente Debrun <debrunvicente@gmail.com>
2023-04-23 19:42:05 -0700

    add task 6

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,18 @@
+
+#!/usr/bin/node
+const args = process.argv.slice(2);
+const request = require('request');
+request(args[0], function (error, response, body) {
+  if (!error) {
+    const todos = JSON.parse(body);
+    const tasks = {};
+    todos.forEach((todo) => {
+      if (todo.completed && !tasks[todo.userId]) {
+        tasks[todo.userId] = 1;
+      } else if (todo.completed && tasks[todo.userId]) {
+        tasks[todo.userId] += 1;
+      }
+    });
+    console.log(tasks);
+  }
+});
